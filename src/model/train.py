4a4c1d04df39b04f709222868011297a7c796e23
sergeik453 <154073847+sergeik453@users.noreply.github.com>
2024-08-29 13:46:53 +0200

    Update train.py
    changed X and y extraction in Split_data

--- src/model/train.py
+++ src/model/train.py
@@ -39,7 +39,9 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X = df.drop('Y', axis=1).values
+    y = df['Y'].values
+    # X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
     return X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
